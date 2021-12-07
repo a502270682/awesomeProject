1b2754ebbf7857e43d2ed2e20883ef180ed28f7b
feiyangguo <feiyang.guo@iglooinsure.com>
2021-12-07 18:55:23 +0800

    FEAT: add flag usage

--- main.go
+++ main.go
@@ -1,36 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"runtime"
-	"time"
 )
 
-var tr *http.Transport
-
-func init() {
-	tr = &http.Transport{
-		MaxIdleConns: 100,
-	}
-}
-
-func test(a Input) *Input {
-
-	fmt.Printf("%x\n", &a)
-	a.E = 4
-	return &a
-}
+type Cent int64
 
 func main() {
 	getGongjijin()
+
 }
 
 func Encrypt(text string, key []byte) (string, error) {
@@ -78,24 +61,17 @@ func getGongjijin() {
 	aTime := 16
 	bTime := 5
 	//cTime := jiao - aTime - bTime
-	for i:=jiao; i > 0; i-- {
-		if i >= jiao - aTime {
+	for i := jiao; i > 0; i-- {
+		if i >= jiao-aTime {
 			sum += a * ratio * float64(i)
-		} else if i >= jiao - aTime - bTime {
+		} else if i >= jiao-aTime-bTime {
 			sum += b * ratio * float64(i)
 		} else {
 			sum += c * ratio * float64(i)
 		}
 	}
-	fmt.Println(sum)
+	fmt.Println("交的月数：", jiao, "贷款额：", sum)
 }
-
-func getmap() map[int64][]*string {
-	var ret map[int64][]*string
-	fmt.Println(ret[1])
-	return ret
-}
-
 
 type TreeNode struct {
 	Val   int
@@ -143,31 +119,3 @@ type Output struct {
 }
 
 type Action func(ctx context.Context, input *Input) (*Output, error)
-
-func Get(url string) ([]byte, error) {
-	m := make(map[string]interface{})
-	data, err := json.Marshal(m)
-	if err != nil {
-		return nil, err
-	}
-	body := bytes.NewReader(data)
-	req, _ := http.NewRequest("GET", url, body)
-	req.Header.Add("content-type", "application/json")
-
-	client := &http.Client{
-		Transport: tr,
-		Timeout:   3 * time.Second,
-	}
-
-	res, err := client.Do(req)
-	if err != nil || res == nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	resBody, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	return resBody, nil
-}
--- proto/client/client.go
+++ proto/client/client.go
@@ -7,25 +7,23 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"log"
 	"time"
 )
 
-func main(){
+func main() {
 	conn, err := grpc.Dial("localhost:6001", grpc.WithInsecure())
 	if err != nil {
 		return
 	}
-	defer  conn.Close()
+	defer conn.Close()
 	c := proto.NewPeopleServiceClient(conn)
 	fmt.Println("start", time.Now())
+
 	// 自带关闭连接请求，避免grpc server端长链接一直没有响应，拖跨client端
-	clientDeadline := time.Now().Add(2 * time.Second)
-	ctx, cancel := context.WithDeadline(context.Background(), clientDeadline)
-	defer cancel()
-	r, err := c.CreatePeople(ctx, &proto.CreatePeopleReq{
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(2*time.Second))
+	_, err = c.CreatePeople(ctx, &proto.CreatePeopleReq{
 		Name: "gfy",
-		Age: 100,
+		Age:  100,
 	})
 	if err != nil {
 		//获取错误状态
@@ -33,11 +31,12 @@ func main(){
 		if ok {
 			//判断是否为调用超时
 			if state.Code() == codes.DeadlineExceeded {
-				log.Fatalln("Route timeout!", time.Now())
+				fmt.Println("Route timeout!", time.Now())
+				cancel()
 			}
 		}
-		log.Fatalf("Call Route err: %v", err)
+		cancel()
+		fmt.Println("Call Route err: ", err)
 	}
 	fmt.Println("end", time.Now())
-	fmt.Println("client", r.Message, r.Ok)
 }
