d6f69aee9dd27225bc356a119a347160d9f31f13
feiyangguo <feiyang.guo@iglooinsure.com>
2022-08-04 13:14:41 +0800

    cron: add init method

--- cron/cron.go
+++ cron/cron.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
+	"github.com/pkg/errors"
 	"github.com/robfig/cron/v3"
 	"log"
 	"os"
 	"time"
 )
 
+var cronCtl Ctl
+var vnLocal *time.Location
+
+type Ctl struct {
+	C *cron.Cron
+}
+
+func init() {
+	var err error
+	vnLocal, err = time.LoadLocation("Asia/Ho_Chi_Minh")
+	if err != nil {
+		panic(errors.Wrap(err, "failed to LoadLocation"))
+	}
+	cronCtl.C = cron.New(cron.WithSeconds(), cron.WithLocation(vnLocal), cron.WithLogger(
+		cron.VerbosePrintfLogger(log.New(os.Stdout, "cron: ", log.LstdFlags))))
+}
+
 func main() {
 	// withSeconds 才能 设置6位参数
-	c := cron.New(cron.WithSeconds(), cron.WithLogger(
-		cron.VerbosePrintfLogger(log.New(os.Stdout, "cron: ", log.LstdFlags))))
+	//c := cron.New(cron.WithSeconds(), cron.WithLogger(
+	//	cron.VerbosePrintfLogger(log.New(os.Stdout, "cron: ", log.LstdFlags))))
 	channel := make(chan int, 0)
 	jobWorker := GreetingJob{
 		Name: "gfy",
@@ -21,14 +39,14 @@ func main() {
 	//entryId, _ := c.AddFunc("@every 5s", jobWorker.Run)
 
 	// AddJob
-	entryId, err := c.AddJob("0 30 0 2 * *", &jobWorker) // cron.NewChain(cron.Recover(cron.DefaultLogger)).Then(&jobWorker)
+	entryId, err := cronCtl.C.AddJob("0 36 17 3 * *", &jobWorker) // cron.NewChain(cron.Recover(cron.DefaultLogger)).Then(&jobWorker)
 	if err != nil {
 		panic(err)
 	}
 	// 支持linux中时间规则 * * * * *（分钟，小时，天，周，月）
 	fmt.Println(fmt.Sprintf("my job id is %d", entryId))
 	//c.AddJob("@every 1s", cron.NewChain(cron.Recover(cron.DefaultLogger)).Then(&panicJob{}))
-	c.Start()
+	cronCtl.C.Start()
 	<-channel
 }
 
